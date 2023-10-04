83f3c206ae3f0f3e831212a6be5cea6f11329a89
Sakina Mbatha <sakina@natix.io>
2023-10-04 15:03:11 +0200

    feat: short command
    to build a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Hey there',
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.deepPurpleAccent[700],
+          backgroundColor: Colors.blue[700],
           title: const Center(
             child: Text(
               'Stranger things',
@@ -24,6 +33,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
