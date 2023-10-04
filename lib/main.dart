fd3677549458b240514d3c3222e5bf1c1b91162c
Sakina Mbatha <sakina@natix.io>
2023-10-04 14:17:38 +0200

    style: design colors

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,21 @@ void main() {
         title: 'Hey there',
         home: Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.deepPurpleAccent[700],
             title: const Center(
               child: Text(
                 'Stranger things',
               ),
             ),
           ),
-          body: const Center(
+          body: Center(
             child: Text(
               textAlign: TextAlign.center,
               'Become an android developer',
               style: TextStyle(
                 fontSize: 26,
                 fontWeight: FontWeight.bold,
+                backgroundColor: Colors.purple[50],
               ),
             ),
           ),
