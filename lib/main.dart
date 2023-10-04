7d22452944a244f9dfc5c93930cfdc63c765f0d1
Sakina Mbatha <sakina@natix.io>
2023-10-04 14:03:13 +0200

    feat: set an appbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      title: 'Hey there',
-      home: Center(
-        child: Text(
-          'Mobile Developer',
-          textAlign: TextAlign.center,
-        ),
-      ),
-    ),
+    MaterialApp(
+        title: 'Hey there',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Stranger things'),
+          ),
+        )),
   );
 }
